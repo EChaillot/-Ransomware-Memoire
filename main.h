bbcd1ae7e5f5c8c1bbf40099e51750de8a15c5be
EChaillot <evann.chaillot@gmail.com>
2024-06-27 22:26:16 +0200

    Add files via upload

---
+++ main.h
@@ -0,0 +1,11 @@
+//
+// Created by user on 11/12/2023.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+const WCHAR *nilah[] = {L"explorer.exe",L"tasklist.exe",L"cmd.exe",L"taskhostw.exe",L"procexp"};
+
+//const WCHAR *nilah[] = {L""};
+#endif //MAIN_H
